2d637c841681614f4090660ae651b8ee074efb89
yakubu rufai mahatma <yrufai23@gmail.com>
2020-06-30 17:20:16 +0000

    Updates

---
+++ precache-manifest.d9774747983d697f061014c54e150ac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692b0b46fad615685cd709c560eb6db0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da0e3105e3797ee0f600",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9680ee62bee18642f9b",
+    "url": "/static/js/2.9b83ecc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b83ecc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0e3105e3797ee0f600",
+    "url": "/static/js/main.80c43df7.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
